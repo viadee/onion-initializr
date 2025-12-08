9483f78d0a6de60f459f8b4d03eab08ac7c3d7c8
Leon Licher <Leon.Licher@viadee.de>
2025-12-08 09:22:58 +0100

    fix: update package.json paths and clean pnpm-lock.yaml

--- cli/fix-lib-package.js
+++ cli/fix-lib-package.js
@@ -1,15 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-const packageJsonPath = path.join(__dirname, 'dist', 'node_modules', '@onion-initializr', 'lib', 'package.json');
+const packageJsonPath = path.join(
+  __dirname,
+  'dist',
+  'node_modules',
+  '@onion-initializr',
+  'lib',
+  'package.json'
+);
 
 if (fs.existsSync(packageJsonPath)) {
   const pkg = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-  
+
   // Fix paths - remove 'dist/' prefix
   if (pkg.main) pkg.main = pkg.main.replace(/^dist\//, '');
   if (pkg.types) pkg.types = pkg.types.replace(/^dist\//, '');
-  
+
   if (pkg.exports) {
     const newExports = {};
     for (const [key, value] of Object.entries(pkg.exports)) {
@@ -17,7 +24,7 @@ if (fs.existsSync(packageJsonPath)) {
     }
     pkg.exports = newExports;
   }
-  
+
   fs.writeFileSync(packageJsonPath, JSON.stringify(pkg, null, 2));
   console.log('✓ Fixed lib package.json exports');
 } else {
