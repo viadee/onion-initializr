d1a0de9c8dc768f949407bbd12fbda4e97c8ad26
mariusz-viadee <mariusz.seget@viadee.com>
2026-09-03 10:56:22 +0200

    add build script for saving the package.json version and resolve file path dependency

--- cli/src/application/services/version-app-service.ts
+++ cli/src/application/services/version-app-service.ts
@@ -1,25 +1,9 @@
-import { PathAppService } from '@onion-initializr/lib/application/services/path-app-service';
-import { FileService } from '@onion-initializr/lib/domain/services/file-service';
+import { CLI_VERSION } from '../configuration/generated-version';
 
 export class VersionAppService {
-  constructor(
-    private fileService: FileService,
-    private pathService: PathAppService
-  ) {}
-
   async handleVersion(): Promise<void> {
     if (process.argv.includes('--version') || process.argv.includes('-v')) {
-      const packageJsonPath = this.pathService.join(
-        __dirname,
-        '../../../../package.json'
-      );
-
-      const originalPackageJson =
-        await this.fileService.readFile(packageJsonPath);
-
-      const parsedPackageJson = JSON.parse(originalPackageJson.content);
-      const onionVersion = parsedPackageJson.version;
-      console.log(`Onion Architecture Generator v${onionVersion}`);
+      console.log(`Onion Architecture Generator v${CLI_VERSION}`);
       process.exit(0);
     }
   }
